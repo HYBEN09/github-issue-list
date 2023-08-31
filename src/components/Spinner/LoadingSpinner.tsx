37d5c29df676b9be23092a8ccc6fdb926bb17ee7
hyebeen byun <hyebeen2658@naver.com>
2023-08-31 11:30:47 +0900

    Feat: LoadingSpinner component 구현

---
+++ src/components/Spinner/LoadingSpinner.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const LoadingSpinnerContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  transform: translateY(120%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+
+const LoadingSpinner = () => {
+  return (
+    <LoadingSpinnerContainer>
+      <img src="/assets/loading.svg" alt="" />
+    </LoadingSpinnerContainer>
+  );
+};
+
+export default LoadingSpinner;
